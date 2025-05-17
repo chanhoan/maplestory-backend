6d203868ec5a84a79c2ffc642b772800a29f8b0c
정찬환 <chanhoan01@gmail.com>
2025-05-17 16:06:52 +0900

    style: 코드 스타일 및 환경설정 변수 타입 명확화

--- gateway/src/config/configuration.ts
+++ gateway/src/config/configuration.ts
@@ -1,11 +1,11 @@
 import { createSecretKey } from 'crypto';
 
 export default () => ({
-  port: parseInt(process.env.PORT, 10),
+  port: parseInt(process.env.PORT!, 10),
 
   jwt: {
     secret: createSecretKey(
-      Buffer.from(process.env.JWT_SECRET_BASE64, 'base64'),
+      Buffer.from(process.env.JWT_SECRET_BASE64!, 'base64'),
     ),
   },
 
